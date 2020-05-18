349fdedea3635f252a580047f799de1d94bc5916
joelz575 <56939394+joelz575@users.noreply.github.com>
2020-05-18 11:39:55 -0400

    travis.yml file was added

--- source_codes/install.sh
+++ source_codes/install.sh
@@ -1,8 +1,23 @@
+$ cat install
 git clone https://github.com/json-c/json-c.git
 #json-c BUILD instructions...
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "cygwin" ]]; then
+   ./bootstrap-vcpkg.sh
+   ./vcpkg integrate install
+else
+
+   .\bootstrap-vcpkg.sh
+   .\vcpkg integrate install
+fi
+echo "vcpkg installed successfully"
+vcpkg install json-c
+echo "json-c installed successfully"
 #für APPVEYOR
 #need .yml file
 make
+echo ".exe made successfully"
 
 
 

6d14e7c63c216040843dcc055fc5da1a82363c19
joelz575 <56939394+joelz575@users.noreply.github.com>
2020-05-22 14:15:03 -0400

    .travis.yml modified

--- source_codes/install.sh
+++ source_codes/install.sh
@@ -3,24 +3,24 @@
 #json-c BUILD instructions...
 
 if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "cygwin" ]]; then
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get update -y
-    sudo apt-get install g++-7 -y
-    sudo apt install git
-    sudo apt install cmake
-    $ git clone https://github.com/json-c/json-c.git
-    $ mkdir json-c-build
-    $ cd json-c-build
-    $ cmake ../json-c
-    $ make
-    $ make test
-    $ make install
+    #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    #sudo apt-get update -y
+    #sudo apt-get install g++-7 -y
+    #sudo apt install git
+    #sudo apt install cmake
+    git clone https://github.com/json-c/json-c.git
+    mkdir json-c-build
+    cd json-c-build
+    cmake ../json-c
+    make
+    make test
+    make install
    #./bootstrap-vcpkg.sh
    #./vcpkg integrate install
 else
    git clone https://github.com/Microsoft/vcpkg.git
-   cd vcpkg
    git clone https://github.com/json-c/json-c.git
+   cd vcpkg
    ./bootstrap-vcpkg.sh
    ./vcpkg integrate install
    vcpkg install json-c
@@ -30,6 +30,7 @@ fi
 #echo "json-c installed successfully"
 #für APPVEYOR
 #need .yml file
+cd ..
 make
 #echo ".exe made successfully"
 

849b2315584e2f418bdd9bf0e2155ab59489a51c
joelz575 <56939394+joelz575@users.noreply.github.com>
2020-05-22 14:01:02 -0400

    install.sh modified

--- source_codes/install.sh
+++ source_codes/install.sh
@@ -1,18 +1,32 @@
 #$ cat install
-git clone https://github.com/json-c/json-c.git
-#json-c BUILD instructions...
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "cygwin" ]]; then
-   ./bootstrap-vcpkg.sh
-   ./vcpkg integrate install
-else
 
+#json-c BUILD instructions...
+
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "cygwin" ]]; then
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo apt-get update -y
+    sudo apt-get install g++-7 -y
+    sudo apt install git
+    sudo apt install cmake
+    $ git clone https://github.com/json-c/json-c.git
+    $ mkdir json-c-build
+    $ cd json-c-build
+    $ cmake ../json-c
+    $ make
+    $ make test
+    $ make install
+   #./bootstrap-vcpkg.sh
+   #./vcpkg integrate install
+else
+   git clone https://github.com/Microsoft/vcpkg.git
+   cd vcpkg
+   git clone https://github.com/json-c/json-c.git
    ./bootstrap-vcpkg.sh
    ./vcpkg integrate install
+   vcpkg install json-c
 fi
 #echo "vcpkg installed successfully"
-vcpkg install json-c
+
 #echo "json-c installed successfully"
 #für APPVEYOR
 #need .yml file

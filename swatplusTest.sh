82b4bbe53b5e7ddb69f26fc145a6afeee046b087
joelz575 <joel.harms@mail.mcgill.ca>
2020-07-28 13:48:17 -0400

    fix some syntax

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,4 +1,5 @@
-pip install git+https://github.com/joelz575/Tinamit
+pip install https://github.com/joelz575/Tinamit
+from Tinamit import test.py
 python3 test.py
-cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
-sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe
+#cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
+#sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe

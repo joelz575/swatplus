5c0f6bcd28e7c403014e8f0c628190e2939eea2d
joelz575 <joel.harms@mail.mcgill.ca>
2020-07-28 12:40:52 -0400

    Travis installed python

---
+++ swatplusTest.sh
@@ -0,0 +1,6 @@
+
+pip install git+https://github.com/joelz575/Tinamit
+#find correct link to tinamit
+python3 test.py
+cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
+sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe

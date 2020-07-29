44d812a661bd5aca491f1a643db40adae4fa474a
joelz575 <joel.harms@mail.mcgill.ca>
2020-07-29 12:37:59 -0400

    change python alias to python3 for mac osx and tried to reach test.py differently

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,5 +1,5 @@
 pip install git+https://github.com/joelz575/Tinamit
-from Tinamit import test.py
-python3 test.py
+#from Tinamit import test.py
+python3 Tinamit/test.py
 #cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
 #sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe

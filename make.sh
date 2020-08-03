1cfec0a90d0a2bf6396253a6a7beed3d33d6a000
joelz575 <56939394+joelz575@users.noreply.github.com>
2020-08-03 16:01:37 -0400

    travis.yml and make.sh update

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ cd build
 cmake .
 cmake   -G "MinGW Makefiles" -D CMAKE_C_COMPILER='C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/gcc.exe' DCMAKE_FFLAGS=-m64 DCMAKE_SH="CMAKE_SH-NOTFOUND"  ../src
 cmake --build .
-cmd /k
+#cmd /k

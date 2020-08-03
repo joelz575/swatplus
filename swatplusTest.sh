215e770891fc2881c6c4960550a55095ecb6d82c
joelz575 <joel.harms@mail.mcgill.ca>
2020-08-03 19:44:46 -0400

    swatplusTest.sh was updated to help with windows

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,6 +1,14 @@
+#if statement for operating systems
+if [[ $TRAVIS_OS_NAME == windows ]]
+then
+python -m pip install --upgrade setuptools
+python -m pip install git+https://github.com/joelz575/Tinamit
+python test.py
+else
 python3 -m pip install --upgrade setuptools
 python3 -m pip install git+https://github.com/joelz575/Tinamit
 #from Tinamit import test.py
 python3 test.py #has to be in swatplus not with Tinamit
 #cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
 #sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe
+fi

d7af926e43c15481ab48b9f07856a18b9169c16c
joelz575 <joel.harms@mail.mcgill.ca>
2020-08-03 14:21:56 -0400

    added test.py

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,5 +1,5 @@
-pip3 install git+https://github.com/joelz575/Tinamit
+python3 -m pip install git+https://github.com/joelz575/Tinamit
 #from Tinamit import test.py
-python3 /test.py
+python3 test.py #has to be in swatplus not with Tinamit
 #cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
 #sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe

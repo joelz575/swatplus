44d28693f31300985dc3e9b2f17634231aec25ec
joelz575 <joel.harms@mail.mcgill.ca>
2020-08-06 12:43:57 -0400

    upgrade unix operating systems to python 3.6

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,14 +1,15 @@
 #if statement for operating systems
 if [[ $TRAVIS_OS_NAME == windows ]]
 then
-python -m ensurepip install --upgrade setuptools
-python -m ensurepip install git+https://github.com/joelz575/Tinamit
+python -m ensurepip
+python -m pip install --upgrade setuptools
+python -m pip install git+https://github.com/joelz575/Tinamit
 python test.py
 else
-python3.9 -m pip install --upgrade setuptools
-python3.9 -m pip install git+https://github.com/joelz575/Tinamit
+python3.6 -m pip install --upgrade setuptools
+python3.6 -m pip install git+https://github.com/joelz575/Tinamit
 #from Tinamit import test.py
-python3.9 test.py #has to be in swatplus not with Tinamit
+python3.6 test.py #has to be in swatplus not with Tinamit
 #cd "/home/joelz/Documents/Prof Adamowski/Iximulew/SWAT+/TRIAL/Trial Robit/Trial Robit/Scenarios/Default/TxtInOut"
 #sudo /home/joelz/PycharmProjects/swatplus/build/bin/swatplus_exe
 fi

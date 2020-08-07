72a4c6cb206153d89d71b2b035a0603911bcf337
joelz575 <joel.harms@mail.mcgill.ca>
2020-08-07 16:08:12 -0400

    upgrade pip

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -7,6 +7,7 @@ python -m pip install --upgrade setuptools
 python -m pip install git+https://github.com/joelz575/Tinamit
 python test.py
 else
+python3.6 -m pip install --upgrade pip
 python3.6 -m pip install --upgrade setuptools
 python3.6 -m pip install git+https://github.com/joelz575/Tinamit
 #from Tinamit import test.py

eb639ac8ae4fe39c763eacf54898f0e0451d4ae8
joelz575 <56939394+joelz575@users.noreply.github.com>
2020-08-07 13:45:27 -0400

    added some testing files

---
+++ Trial Robit/Scenarios/Default/TxtInOut/nutrients.sol
@@ -0,0 +1,3 @@
+nutrients.sol: written by SWAT+ editor v1.2.3 on 2020-07-08 09:06
+name                     dp_co         tot_n         min_n         org_n         tot_p         min_p         org_p         sol_p         h3a_p        mehl_p        bray_p  description
+soilnut1              13.00000       6.00000       3.00000       3.00000       3.50000       0.40000       0.15000       0.25000       1.20000       0.85000       0.85000  

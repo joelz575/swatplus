47a582334445ccf9a766968b0d69ade7b78b4b67
joelz575 <joel.harms@mail.mcgill.ca>
2021-05-12 10:52:12 -0400

    use python 3.8

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,4 +1,4 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 export PATH=$HOME/.poetry/bin:$PATH
-poetry install
+poetry install -v env use 3.8
 poetry run pytest -s swatplus_test/test.py

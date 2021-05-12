61e7c96058bc7a92907c82cb858a57a47a9359f0
joelz575 <joel.harms@mail.mcgill.ca>
2021-05-12 10:34:36 -0400

    add poetry to path

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,3 +1,4 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 poetry install
+export PATH=$HOME/.poetry/bin:$PATH
 poetry run pytest -s swatplus_test/test.py

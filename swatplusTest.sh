6345f96c39caf5923dc487e0abe598e3a456c895
joelz575 <joel.harms@mail.mcgill.ca>
2021-05-12 10:24:44 -0400

    poetry fix

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,3 +1,3 @@
-pip3 install poetry
-poetry install -v env use 3.9
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+poetry install
 poetry run pytest -s swatplus_test/test.py

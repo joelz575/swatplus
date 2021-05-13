5bdbf6e40cfd2786a07d5aa10bffed911cb9798f
joelz575 <joel.harms@mail.mcgill.ca>
2021-05-13 15:43:32 -0400

    Making python default language

--- swatplusTest.sh
+++ swatplusTest.sh
@@ -1,6 +1,5 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 export PATH=$HOME/.poetry/bin:$PATH
 poetry env list
-poetry env use 3.9
 poetry install
 poetry run pytest -s swatplus_test/test.py
